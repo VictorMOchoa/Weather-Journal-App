66a524195f7419535f36f81210f48af5c8ef0978
Victor Ochoa <ochoa.m.victor@gmail.com>
2020-05-03 22:50:59 -0700

    Now retrieving weather

--- website/app.js
+++ website/app.js
@@ -1,4 +1,5 @@
 /* Global Variables */
+const apiKey = "API_KEY";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
@@ -8,20 +9,35 @@ let generateBtn = document.getElementById("generate");
 generateBtn.addEventListener("click", onClickGenerate);
 
 function onClickGenerate() {
-  if (isUserInputValid()) {
-    console.log("valid input")
-    getWeatherFromAPI();
-    sendDataToServer();
-    updateUI();
-  } else {
-    console.error("User input is not valid. Please check zip and input.");
-  }
-
-}
-
-function isUserInputValid() {
   let feelings = document.getElementById("feelings").value;
   let zip = document.getElementById("zip").value;
 
+  if (isUserInputValid(feelings, zip)) {
+    getWeatherFromAPI(zip)
+      .then(function (data) {
+        sendDataToServer();
+        updateUI();
+      })
+
+  } else {
+    console.error("User input is not valid. Please check zip and input.");
+  }
+}
+
+function isUserInputValid(feelings, zip) {
   return !isNaN(zip) && zip.length != 0 && feelings.length != 0;
 }
+
+const getWeatherFromAPI = async (zip) => {
+  const response  = await fetch(apiURL + zip + '&appid=' + apiKey + '&units=imperial');
+  try {
+      const data = await response.json();
+      return data;
+  } catch(error) {
+      console.error('error', error);
+  };
+};
+
+const sendDataToServer = async (url, data) => {
+  console.log("placeholder")
+};
